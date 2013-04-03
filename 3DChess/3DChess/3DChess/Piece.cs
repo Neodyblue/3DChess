2133233a7318cbe3ac537315c30a2559d9902e80
Zehir <thibaudmichaud.1@gmail.com>
2013-04-03 14:44:58 +0200

    base du jeu

---
+++ 3DChess/3DChess/3DChess/Piece.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3DChess
+{
+    class Piece
+    {
+        Type type;
+        bool white;
+        public Type Type
+        {
+            get { return type; }
+        }
+        public bool White
+        {
+            get { return white; }
+        }
+
+        public Piece(Type type, bool white)
+        {
+            this.type = type;
+            this.white = white;
+        }
+    }
+}
